53f816c142364d953cea8b96c83e6d368e10a281
faith <huycutety@gmail.com>
2023-11-14 23:39:28 +0700

    login

--- src/views/App.js
+++ src/views/App.js
@@ -1,11 +1,39 @@
-import { Route, Routes } from 'react-router-dom';
 import '../style/App.scss';
+import 'react-toastify/dist/ReactToastify.css';
 import Nav from './nav/Nav';
 import Home from './home/Home';
 import Login from './login/Login';
 import ProductList from './product/ProductList';
 import CartList from './cart/CartList';
+import { ToastContainer, toast } from 'react-toastify';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 function App() {
+
+  const [Users, setUser] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch('account.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+      })
+      .catch((error) => console.log('error reading json', error));
+  }, []);
+
+  //sử lý login
+  const handleLogin = (checklogin) => {
+    const userFound = Users.find((user) => user.username === checklogin.username && user.password === checklogin.password);
+    if (userFound) {
+      navigate('/home');
+      toast.success("Login success!")
+    } else {
+      toast.error("Login failed, check username and password again!")
+    }
+    console.log('Logging in with:', checklogin);
+  }
+
   return (
     <div className="App">
       <nav>
@@ -16,8 +44,20 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/product" element={<ProductList />} />
           <Route path="/shoppingcart" element={<CartList />} />
-          <Route path="/Login" element={<Login />} />
+          <Route path="/Login" element={<Login onLogin={handleLogin} />} />
         </Routes>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </body>
     </div>
   );
--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,10 +1,33 @@
-const Login = () => {
+import "../../style/Login.scss"
+import React, { useState } from 'react';
+const Login = ({ onLogin }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSubmitlogin = (e) => {
+        e.preventDefault();
+        const checklogin = { username, password }
+        onLogin(checklogin);
+        setUsername('');
+        setPassword('');
+    }
+
     return (
-        <>
+        <div className="box">
+            <h1>
+                Login Page
+            </h1>
             <div>
-                <h1>Login Page</h1>
+                <form onSubmit={handleSubmitlogin} >
+                    <div className="ipt-username">
+                        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter your Username" />
+                    </div>
+                    <div className="ipt-password">
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your Password" />
+                    </div>
+                    <div className="btn-login"><button>Login</button></div>
+                </form>
             </div>
-        </>
+        </div>
     )
 }
 export default Login;
